f428bb9fa04a34fbe5485f94bb575b33e8a0f06b
Maria DelSignore <maria.ad27@gmail.com>
2021-09-09 19:55:32 -0600

    Add basic function structure

---
+++ src/SpaceContainer/SpaceContainer.js
@@ -0,0 +1,13 @@
+import './SpaceContainer.css'
+
+const SpaceContainer = ({ epicData }) => {
+  console.log(epicData)
+  return (
+    <article>
+      <h3>hi</h3>
+    </article>
+  )
+}
+
+
+export default SpaceContainer;
