12d499b06255068f0933e440a2733137d041eb04
Maria DelSignore <maria.ad27@gmail.com>
2021-09-09 19:55:01 -0600

    Add header tag and import HomePage

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,14 @@
+import HomePage from '../HomePage/HomePage'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     <h1>y0</h1>
+      <header>
+      <h1>Spacetagram</h1>
+      </header>
+      <HomePage />
     </div>
   );
 }
