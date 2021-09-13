713ade491db904504a2e92eafa7b8aa5397a2580
Maria DelSignore <maria.ad27@gmail.com>
2021-09-12 21:13:15 -0600

    Add class names to img and btn containers

--- src/SpaceCard/SpaceCard.js
+++ src/SpaceCard/SpaceCard.js
@@ -3,12 +3,17 @@ import './SpaceCard.css'
 const SpaceCard = ({ epicData }) => {
   return (
     <article>
-      <img src={epicData.url} alt='this is something' />
+      <div className='img-container'>
+        <img src={epicData.url} alt='this is something' />
+      </div>
+      <div className='btn-container'>
+        <button>Like</button>
+      </div>
       <div className='card-info'>
         <h2>{epicData.title}</h2>
         <h3>{epicData.date}</h3>
         <p>{epicData.explanation}</p>
-        <h4>{epicData.copyright}</h4>
+        {/* <h4>{epicData.copyright}</h4> */}
       </div>
     </article>
   )
