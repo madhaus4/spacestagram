2c0706be03c7090ae0be65415c547a56cfd36ec1
Maria DelSignore <maria.ad27@gmail.com>
2021-09-13 15:19:28 -0600

    Remove like a post prop

--- src/SpaceCard/SpaceCard.js
+++ src/SpaceCard/SpaceCard.js
@@ -1,19 +1,18 @@
 import './SpaceCard.css'
 
-const SpaceCard = ({ epicData, likeAPost }) => {
+const SpaceCard = ({ epicData }) => {
   return (
     <article>
       <div className='img-container'>
         <img src={epicData.url} alt='this is something' />
       </div>
       <div className='btn-container'>
-        <button onClick={() => likeAPost(epicData)}>Like</button>
+        <button>Like</button>
       </div>
       <div className='card-info'>
         <h2>{epicData.title}</h2>
         <h3>{epicData.date}</h3>
         <p>{epicData.explanation}</p>
-        {/* <h4>{epicData.copyright}</h4> */}
       </div>
     </article>
   )
