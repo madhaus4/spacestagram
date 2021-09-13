a46dc12a116a748c236da8a76ae4873f18a02958
Maria DelSignore <maria.ad27@gmail.com>
2021-09-13 15:17:22 -0600

    Implement hooks into home page

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,41 +1,66 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import SpaceContainer from '../SpaceContainer/SpaceContainer';
 import './HomePage.css'
 
-class HomePage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      epicData: [],
-      likedPosts: [],
-    }
-  }
+const HomePage = () => {
+  const [epicData, setEpicData] = useState([])
+  const [FavoritedPosts, setFavoritedPosts] = useState([])
 
-  componentDidMount() {
+  const fetchData = () => {
     fetch('https://api.nasa.gov/planetary/apod?api_key=1xvzSUJvKIPsf7PliA1VJPYWMTVNda8BblYK7z3r&count=10')
       .then(res => res.json())
-      .then(data => this.setState({epicData: data}))
+      .then(data => setEpicData(data))
   }
 
-  likeAPost = (newlyLiked) => {
-    newlyLiked = {
-      id: Date.now() 
-    }
-    this.setState({likedPosts: [...this.state.likedPosts, newlyLiked]})
-  }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-  render() {
-    console.log('liked', this.state.likedPosts)
-    return (
-      <>
-        <SpaceContainer 
-          epicData={this.state.epicData}
-          likeAPost={this.likeAPost}
-        />
-      </>
-    )
-  }
+  return (
+    <>
+      <SpaceContainer 
+        epicData={epicData}
+      />
+    </>
+  )
 }
+
+
+// class HomePage extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       epicData: [],
+//       likedPosts: [],
+//     }
+//   }
+
+  // componentDidMount() {
+  //   fetch('https://api.nasa.gov/planetary/apod?api_key=1xvzSUJvKIPsf7PliA1VJPYWMTVNda8BblYK7z3r&count=10')
+  //     .then(res => res.json())
+  //     .then(data => this.setState({epicData: data}))
+  // }
+
+//   likeAPost = (newlyLiked) => {
+//     newlyLiked = {
+//       id: Date.now() 
+//     }
+//     this.setState({likedPosts: [...this.state.likedPosts, newlyLiked]})
+//   }
+
+//   render() {
+//     console.log('liked', this.state.likedPosts)
+//     return (
+//       <>
+//         <SpaceContainer 
+//           epicData={this.state.epicData}
+//           likeAPost={this.likeAPost}
+//         />
+//       </>
+//     )
+//   }
+// }
 
 
 export default HomePage;
