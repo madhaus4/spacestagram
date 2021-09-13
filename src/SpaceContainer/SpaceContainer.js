0bb6b1ccc65cf6aaf439ca18dc157c28cdf17170
Maria DelSignore <maria.ad27@gmail.com>
2021-09-13 08:17:00 -0600

    Pass like a post prop

--- src/SpaceContainer/SpaceContainer.js
+++ src/SpaceContainer/SpaceContainer.js
@@ -1,10 +1,14 @@
 import './SpaceContainer.css'
 import SpaceCard from '../SpaceCard/SpaceCard'
 
-const SpaceContainer = ({ epicData }) => {
+const SpaceContainer = ({ epicData, likeAPost }) => {
   console.log('epicData', epicData)
   let theEpic = epicData.map(element => {
-    return <SpaceCard epicData={element} key={Date.now()} />
+    return <SpaceCard 
+      key={Date.now()} 
+      epicData={element} 
+      likeAPost={likeAPost}
+    />
   })
   return (
     <main>{theEpic}</main>
