6d077a51eb199bc03be76d919f5b6a0359c7f7cf
Maria DelSignore <maria.ad27@gmail.com>
2021-09-14 16:39:06 -0600

    Move header element to new Header component

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,9 @@
 import HomePage from '../HomePage/HomePage'
 import './App.css';
 
-
 const App = () => {
   return (
     <div className="App">
-      <header>
-        <h1>Spacestagram</h1>
-        <div className='btn-container'>
-          <button className='view-likes-btn'>View Likes</button>
-          <button className='view-all-btn'>View All</button>
-        </div>
-      </header>
       <HomePage />
     </div>
   );
