ab011d879a53eb91920831702801c06c23299df1
Maria DelSignore <maria.ad27@gmail.com>
2021-09-14 16:32:44 -0600

    Add class name to div in header

--- src/App/App.js
+++ src/App/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <div className="App">
       <header>
         <h1>Spacestagram</h1>
-        <div>
+        <div className='btn-container'>
           <button className='view-likes-btn'>View Likes</button>
           <button className='view-all-btn'>View All</button>
         </div>
