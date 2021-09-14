718448d8ac39715d5489b720b9878dabdd1e6126
Maria DelSignore <maria.ad27@gmail.com>
2021-09-14 16:57:34 -0600

    Pass in toggle favorites prop

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,16 +1,18 @@
 import './Header.css'
 
-function Header() {
+function Header(props) {
   return (
     <header>
     <h1>Spacestagram</h1>
     <div className='btn-container'>
       <button 
         className='view-likes-btn'
+        onClick={() => props.toggleFavoritesDisplay()}
         >View Likes
       </button>
       <button 
         className='view-all-btn'
+        onClick={() => props.toggleFavoritesDisplay()}
         >View All
       </button>
     </div>
