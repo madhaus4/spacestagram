4f1ef947723dc1f9ac146be11f0e3face44fd97d
Maria DelSignore <maria.ad27@gmail.com>
2021-09-15 07:57:14 -0600

    Add ternary logic to render favorites or all posts

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,4 +1,3 @@
-// import { Component } from 'react';
 import React, { useState, useEffect } from 'react';
 import SpaceContainer from '../SpaceContainer/SpaceContainer';
 import Header from '../Header/Header';
@@ -38,49 +37,13 @@ const HomePage = () => {
         toggleFavoritesDisplay={toggleFavoritesDisplay}
       />
       <SpaceContainer 
-        epicData={epicData}
+        epicData={!isFavorited ? epicData : favoritedPosts}
         addToFavorites={addToFavorites}
         favoritedPosts={favoritedPosts}
       />
     </>
   )
 }
-
-
-// class HomePage extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       epicData: [],
-//       likedPosts: [],
-//     }
-//   }
-
-  // componentDidMount() {
-  //   fetch('https://api.nasa.gov/planetary/apod?api_key=1xvzSUJvKIPsf7PliA1VJPYWMTVNda8BblYK7z3r&count=10')
-  //     .then(res => res.json())
-  //     .then(data => this.setState({epicData: data}))
-  // }
-
-//   likeAPost = (newlyLiked) => {
-//     newlyLiked = {
-//       id: Date.now() 
-//     }
-//     this.setState({likedPosts: [...this.state.likedPosts, newlyLiked]})
-//   }
-
-//   render() {
-//     console.log('liked', this.state.likedPosts)
-//     return (
-//       <>
-//         <SpaceContainer 
-//           epicData={this.state.epicData}
-//           likeAPost={this.likeAPost}
-//         />
-//       </>
-//     )
-//   }
-// }
 
 
 export default HomePage;
