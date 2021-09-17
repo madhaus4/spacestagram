be5274394e8d8382cd4c0b6695fca489443081bc
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 16:53:07 -0600

    Return the fetch call

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,6 +1,4 @@
-const getData = () => {
-  fetch('https://api.nasa.gov/planetary/apod?api_key=1xvzSUJvKIPsf7PliA1VJPYWMTVNda8BblYK7z3r&count=10')
+export const getData = () => {
+  return fetch('https://api.nasa.gov/planetary/apod?api_key=1xvzSUJvKIPsf7PliA1VJPYWMTVNda8BblYK7z3r&count=10')
   .then(res => res.json())
 }
-
-export default getData;
