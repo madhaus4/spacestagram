5a334209ee3ab7dda8af8996bf83e72b94f4d324
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 14:56:05 -0600

    Update conditional render for button

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,6 @@
 import './Header.css'
 
-function Header({ toggleFavoritesDisplay, isFavorited}) {
+function Header({ toggleFavoritesDisplay, isFavoritedDisplayed }) {
   return (
     <header>
     <h1>Spacestagram</h1>
@@ -8,7 +8,7 @@ function Header({ toggleFavoritesDisplay, isFavorited}) {
       <button 
         className='view-likes-btn'
         onClick={() => toggleFavoritesDisplay()}
-        >{isFavorited ? 'Home' : 'View Favorites'}
+        >{isFavoritedDisplayed ? 'Home' : 'View Favorites'}
       </button>
     </div>
   </header>
