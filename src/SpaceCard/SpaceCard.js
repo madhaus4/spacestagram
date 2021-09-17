abae767eb596a089eec2bde96bfb1398135334bb
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 14:56:48 -0600

    Update conditinal render for button

--- src/SpaceCard/SpaceCard.js
+++ src/SpaceCard/SpaceCard.js
@@ -2,26 +2,28 @@ import './SpaceCard.css'
 import unfavoritedIcon from '../unfavorited.svg'
 import favoritedIcon from '../favorited.svg'
 
-const SpaceCard = ({ epicData, addToFavorites }) => {
-  return (
-    <article>
-      <div className='img-container'>
-        <img src={epicData.url} alt='this is something' />
-      </div>
-      <div className='article-btn-container'>
-        <img
-          className='favorite-btn'
-          onClick={() => addToFavorites(epicData)}
-          src={unfavoritedIcon} 
-        />
-      </div>
-      <div className='card-info'>
-        <h2>{epicData.title}</h2>
-        <h3>{epicData.date}</h3>
-        <p>{epicData.explanation}</p>
-      </div>
-    </article>
-  )
+const SpaceCard = ({ epicData, updateFavorites }) => {
+  // if (epicData.media_type !== 'video') {
+    return (
+      <article>
+        <div className='img-container'>
+          <img src={epicData.url} alt='this is something' />
+        </div>
+        <div className='article-btn-container'>
+          <img
+            className='favorite-btn'
+            onClick={() => updateFavorites(epicData)}
+            src={!epicData.isFavorited ? unfavoritedIcon : favoritedIcon} 
+          />
+        </div>
+        <div className='card-info'>
+          <h2>{epicData.title}</h2>
+          <h3>{epicData.date}</h3>
+          <p>{epicData.explanation}</p>
+        </div>
+      </article>
+    )
+  // }
 }
 
 
