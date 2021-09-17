8449016d5bb5274ce7322b2df141122e1d0fe3b4
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 14:56:27 -0600

    Implement local storage for favorites to persist

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { cleanData } from '../utilities'
 import SpaceContainer from '../SpaceContainer/SpaceContainer';
 import Header from '../Header/Header';
 import './HomePage.css'
@@ -6,40 +7,80 @@ import './HomePage.css'
 const HomePage = () => {
   const [epicData, setEpicData] = useState([])
   const [favoritedPosts, setFavoritedPosts] = useState([])
-  const [isFavorited, setIsFavorited] = useState(false)
+  const [isFavoritedDisplayed, setisFavoritedDisplayed] = useState(false)
 
   const fetchData = () => {
     fetch('https://api.nasa.gov/planetary/apod?api_key=1xvzSUJvKIPsf7PliA1VJPYWMTVNda8BblYK7z3r&count=10')
       .then(res => res.json())
-      .then(data => setEpicData(data))
+      .then(data => setEpicData(cleanData(data)))
   }
 
   useEffect(() => {
-    fetchData()
+      fetchData()
+      retrieveFromStorage()
   }, [])
+
+  const handleFavorite = (favorite) => {
+    // console.log('favorite++ ', favorite)
+    const foundPost = epicData.find(post => post.title === favorite.title)
+    // console.log('foundPost++ ', foundPost)
+    foundPost.isFavorited = !foundPost.isFavorited
+  }
+
+  const updateFavorites = (post) => {
+    let foundFavorite = favoritedPosts.find(favorite => favorite.id === post.id)
+
+    foundFavorite ? removeFromFavorites(foundFavorite) : addToFavorites(post)
+  }
 
   const addToFavorites = (post) => {
     let likedPost = {
-      id: Date.now(),
+      id: `${post.title} ${post.date}`,
+      isFavorited: true,
       ...post
     }
+    // console.log('likedPost>>> ', likedPost)
+    saveToStorage(likedPost)
     setFavoritedPosts([...favoritedPosts, likedPost])
+    handleFavorite(likedPost)
+  }
+
+  const removeFromFavorites = (post) => {
+    let currentPosts = favoritedPosts.filter(favorite => favorite.title !== post.title)
+    deleteFromStorage(post)
+    setFavoritedPosts(currentPosts);
   }
 
   const toggleFavoritesDisplay = () => {
-    setIsFavorited(!isFavorited)
+    setisFavoritedDisplayed(!isFavoritedDisplayed)
+  }
+
+  // LOCAL STORAGE
+  const saveToStorage = (post) => {
+    localStorage.setItem(post.id, JSON.stringify(post))
+  }
+
+  const retrieveFromStorage = () => {
+    const getStoredPosts = Object.keys(localStorage).map(post => {
+      return JSON.parse(localStorage.getItem(post))
+    })
+    setFavoritedPosts(getStoredPosts)
+  }
+
+  const deleteFromStorage = (post) => {
+    localStorage.removeItem(post.id)
   }
 
   return (
     <>
       <Header 
-        isFavorited={isFavorited}
+        isFavoritedDisplayed={isFavoritedDisplayed}
         toggleFavoritesDisplay={toggleFavoritesDisplay}
       />
       <SpaceContainer 
-        epicData={!isFavorited ? epicData : favoritedPosts}
-        addToFavorites={addToFavorites}
-        favoritedPosts={favoritedPosts}
+        epicData={!isFavoritedDisplayed ? epicData : favoritedPosts}
+        isFavoritedDisplayed={isFavoritedDisplayed}
+        updateFavorites={updateFavorites}
       />
       <footer>
         <p className='footer-text'>Thanks for visiting!</p>
