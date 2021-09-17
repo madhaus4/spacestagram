b3e8ce5b76ebd52a566964788434609a66413e91
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 16:58:14 -0600

    Update filter conditional

--- src/components/SpaceContainer/SpaceContainer.js
+++ src/components/SpaceContainer/SpaceContainer.js
@@ -2,7 +2,8 @@ import SpaceCard from '../SpaceCard/SpaceCard'
 import './SpaceContainer.css'
 
 const SpaceContainer = ({ epicData, isFavoritedDisplayed, updateFavorites }) => {
-  const filteredData = epicData.filter(element => element.media_type !== 'video' || element.media_type !== 'other')
+  const filteredData = epicData.filter(element => element.media_type !== 'video' && element.media_type !== 'other')
+  
   const theEpic = filteredData.map(element => {
     return <SpaceCard 
       key={element.title + element.date} 
