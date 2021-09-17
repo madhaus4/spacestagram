58b1c903d4811f4673646485d0140d268afcaeac
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 16:53:18 -0600

    Update let to const

--- src/components/SpaceContainer/SpaceContainer.js
+++ src/components/SpaceContainer/SpaceContainer.js
@@ -1,16 +1,15 @@
-import './SpaceContainer.css'
 import SpaceCard from '../SpaceCard/SpaceCard'
+import './SpaceContainer.css'
 
 const SpaceContainer = ({ epicData, isFavoritedDisplayed, updateFavorites }) => {
-  let theEpic = epicData.map(element => {
-    if (element.media_type !== 'video') {
-      return <SpaceCard 
-        key={element.title + element.date} 
-        epicData={element} 
-        updateFavorites={updateFavorites}
-        isFavoritedDisplayed={isFavoritedDisplayed}
-      />
-    }
+  const filteredData = epicData.filter(element => element.media_type !== 'video' || element.media_type !== 'other')
+  const theEpic = filteredData.map(element => {
+    return <SpaceCard 
+      key={element.title + element.date} 
+      epicData={element} 
+      updateFavorites={updateFavorites}
+      isFavoritedDisplayed={isFavoritedDisplayed}
+    />
   });
 
   return (
