0cd3cb53bada38ef228a3fd735efee6da966e60b
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 18:57:52 -0600

    Add button animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,42 @@ function Header({ toggleFavoritesDisplay, isFavoritedDisplayed }) {
   return (
     <header>
     <h1 className='tracking-in-contract-bck'>Spacestagram</h1>
-    <div className='btn-container'>
+
+    <div class="container">
+      <div class="center">
+        <button class="btn"
+          onClick={() => toggleFavoritesDisplay()}>
+          <svg width="180px" height="35px" viewBox="0 0 180 35" class="border">
+            <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+            <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+          </svg>
+          <span>{isFavoritedDisplayed ? 'Home' : 'View Favorites'}</span>
+        </button>
+      </div>
+    </div>
+
+    {/* <div className='btn-container'>
       <button 
         className='view-likes-btn'
         onClick={() => toggleFavoritesDisplay()}
         >{isFavoritedDisplayed ? 'Home' : 'View Favorites'}
       </button>
-    </div>
+    </div> */}
   </header>
   )
 }
 
 export default Header;
+
+
+{/* <div class="container">
+  <div class="center">
+    <button class="btn">
+      <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+        <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+        <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+      </svg>
+      <span>HOVER ME</span>
+    </button>
+  </div>
+</div> */}
