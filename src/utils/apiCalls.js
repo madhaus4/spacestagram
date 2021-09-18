6535026c8d7143d2e679bc24495cd80f5df15640
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 22:47:30 -0600

    Update count on api to 50

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,4 +1,4 @@
 export const getData = () => {
-  return fetch('https://api.nasa.gov/planetary/apod?api_key=1xvzSUJvKIPsf7PliA1VJPYWMTVNda8BblYK7z3r&count=10')
+  return fetch('https://api.nasa.gov/planetary/apod?api_key=1xvzSUJvKIPsf7PliA1VJPYWMTVNda8BblYK7z3r&count=50')
   .then(res => res.json())
 }
