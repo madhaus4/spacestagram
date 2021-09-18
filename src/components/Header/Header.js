0b592d0e968f6ec8dc906cd5d9225e9848c98ad7
Maria DelSignore <maria.ad27@gmail.com>
2021-09-17 22:42:50 -0600

    Add alt text and update classes to classnames

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,15 +6,15 @@ function Header({ toggleFavoritesDisplay, isFavoritedDisplayed }) {
   return (
     <header>
     {/* <h1 className='tracking-in-contract-bck'>Spacestagram</h1> */}
-    <img className='spacesta-logo-header' src={logo} />
+    <img className='spacesta-logo-header' src={logo} alt='spacestagram logo white text and white semi circle around a planet that is purple, blue and pink ish' />
     <p>Brought to you by NASA's image API</p>
-    <div class="container">
-      <div class="center">
-        <button class="btn"
+    <div className="container">
+      <div className="center">
+        <button className="btn"
           onClick={() => toggleFavoritesDisplay()}>
-          <svg width="180px" height="35px" viewBox="0 0 180 35" class="border">
-            <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
-            <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+          <svg width="180px" height="35px" viewBox="0 0 180 35" className="border">
+            <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
+            <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
           </svg>
           <span>{isFavoritedDisplayed ? 'Home' : 'View Favorites'}</span>
         </button>
